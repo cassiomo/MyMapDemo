119c50c90f33f57559f4572761c9f2e441f403f2
Kevin Mo <cassinomo@yahoo.com>
2014-10-14 22:33:00 -0700

    alll files

--- src/com/example/mapdemo/MapDemoActivity.java
+++ src/com/example/mapdemo/MapDemoActivity.java
@@ -1,14 +1,22 @@
 package com.example.mapdemo;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.animation.BounceInterpolator;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -18,12 +26,21 @@ import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.InfoWindowAdapter;
+import com.google.android.gms.maps.GoogleMap.OnInfoWindowClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapDemoActivity extends FragmentActivity implements
 		GooglePlayServicesClient.ConnectionCallbacks,
-		GooglePlayServicesClient.OnConnectionFailedListener {
+		GooglePlayServicesClient.OnConnectionFailedListener,
+		OnMapLongClickListener,
+		OnInfoWindowClickListener{
 
 	private SupportMapFragment mapFragment;
 	private GoogleMap map;
@@ -52,6 +69,9 @@ public class MapDemoActivity extends FragmentActivity implements
 			Toast.makeText(this, "Error - Map Fragment was null!!", Toast.LENGTH_SHORT).show();
 		}
 
+		map.setOnMapLongClickListener(this);
+		map.setOnInfoWindowClickListener(this);
+		map.setInfoWindowAdapter(new CustomWindowAdapter(getLayoutInflater()));
 	}
 
 	/*
@@ -204,5 +224,125 @@ public class MapDemoActivity extends FragmentActivity implements
 			return mDialog;
 		}
 	}
+	
+	 @Override
+	    public void onMapLongClick(final LatLng point) {
+	      Toast.makeText(this, "Long Press", Toast.LENGTH_LONG).show();
+	      // Custom code here...
+	      showAlertDialogForPoint(point);
+	    }
 
+	   // Display the alert that adds the marker
+	   private void showAlertDialogForPoint(final LatLng point) {
+	      // inflate message_item.xml view
+	      View  messageView = LayoutInflater.from(MapDemoActivity.this).
+	        inflate(R.layout.message_item, null);
+	      // Create alert dialog builder
+	      AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+	      // set message_item.xml to AlertDialog builder
+	      alertDialogBuilder.setView(messageView);
+
+	      // Create alert dialog
+	      final AlertDialog alertDialog = alertDialogBuilder.create();
+
+	      // Configure dialog button (OK)
+	      alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, "OK", 
+	        new DialogInterface.OnClickListener() {
+	          @Override
+	          public void onClick(DialogInterface dialog, int which) {
+	              // Define color of marker icon
+	              BitmapDescriptor defaultMarker =
+	                 BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN);
+	              // Extract content from alert dialog
+	              String title = ((EditText) alertDialog.findViewById(R.id.etTitle)).
+	                  getText().toString();
+	              String snippet = ((EditText) alertDialog.findViewById(R.id.etSnippet)).
+	                  getText().toString();
+	              // Creates and adds marker to the map
+	              Marker marker = map.addMarker(new MarkerOptions()
+	                .position(point)
+	                .title(title)
+	                .snippet(snippet)       
+	                .icon(defaultMarker));
+	              dropPinEffect(marker);
+	          }
+	      });
+
+	      // Configure dialog button (Cancel)
+	      alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel", 
+	      new DialogInterface.OnClickListener() {
+	        public void onClick(DialogInterface dialog, int id) { dialog.cancel(); }
+	      });
+
+	      // Display the dialog
+	      alertDialog.show();
+	  }
+	   
+	   private void dropPinEffect(final Marker marker) {
+		      // Handler allows us to repeat a code block after a specified delay
+		      final android.os.Handler handler = new android.os.Handler();
+		      final long start = SystemClock.uptimeMillis();
+		      final long duration = 1500;
+
+		      // Use the bounce interpolator
+		      final android.view.animation.Interpolator interpolator = 
+		          new BounceInterpolator();
+
+		      // Animate marker with a bounce updating its position every 15ms
+		      handler.post(new Runnable() {
+		          @Override
+		          public void run() {
+		              long elapsed = SystemClock.uptimeMillis() - start;
+		              // Calculate t for bounce based on elapsed time 
+		              float t = Math.max(
+		                      1 - interpolator.getInterpolation((float) elapsed
+		                              / duration), 0);
+		              // Set the anchor
+		              marker.setAnchor(0.5f, 1.0f + 14 * t);
+		         
+		              if (t > 0.0) {
+		                  // Post this event again 15ms from now.
+		                  handler.postDelayed(this, 15);
+		              } else { // done elapsing, show window
+		                  marker.showInfoWindow();
+		              }
+		          }
+		      });
+		  }
+
+	   class CustomWindowAdapter implements InfoWindowAdapter{
+			LayoutInflater mInflater;
+			
+			public CustomWindowAdapter(LayoutInflater i){
+				mInflater = i;
+			}
+
+		        // This defines the contents within the info window based on the marker
+			@Override
+			public View getInfoContents(Marker marker) {
+			    // Getting view from the layout file
+			    View v = mInflater.inflate(R.layout.custom_info_window, null);
+			    // Populate fields	
+			    TextView title = (TextView) v.findViewById(R.id.tv_info_window_title);
+			    title.setText(marker.getTitle());
+
+			    TextView description = (TextView) v.findViewById(R.id.tv_info_window_description);
+			    description.setText(marker.getSnippet());
+			    // Return info window contents
+			    return v;
+			}
+		        
+		        // This changes the frame of the info window; returning null uses the default frame.
+			@Override
+			public View getInfoWindow(Marker marker) {
+				return null;
+			}
+		}
+
+	@Override
+	public void onInfoWindowClick(Marker arg0) {
+		// TODO Auto-generated method stub
+		Toast.makeText(this, "Info Click", Toast.LENGTH_SHORT).show();
+	}
+	   
 }
